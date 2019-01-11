c583a38c23a9e0c7c31c789f8a4fbf76fa0ce625
Unknown <andrey.bogomolov@tryggel.com>
2019-01-11 17:47:14 +0100

    Fixing error with Initiation every round

--- main.js
+++ main.js
@@ -11,67 +11,67 @@ let CommitteeOfAppropriation = require('Committis');
 let Commune = require('Commune');
 
 
+
 function myCron(numberT, numberD = 0) {
   return (Game.time + numberD) / numberT == Math.round((Game.time + numberD) / numberT)
 }
 
 
-Creep.prototype.kill = function () {
-  // create a balanced body as big as possible with the given energy
-  // create creep with the created body and the given role
-  return this.suicide();
-};
-
-
 let roleHarvester = require('role.harvester');
 let roleBuilder = require('role.builder');
 let roleUpgrader = require('role.upgrader');
+let roleTower = require('role.tower');
 
 
-let ConstructionBuro = new BuroOfConstruction();
-let committee = new CommitteeOfAppropriation();
-committee.focusOnCommune('r');
-let commune = new Commune('r');
-commune.init();
 
 
-let map = new BuroOfCartography();
-let r = map.mapRoom('W3N7');
-console.log(JSON.stringify(commune.creeps));
 
-let CreepsBuro = new BuroOfProduction(commune);
-CreepsBuro.getProductionFacilities();
-
-
-let HarvestBuro = new BuroOfHarvest('r');
-let ProgressBuro = new BuroOfProgress('r');
 
 module.exports.loop = function () {
-
+    let commune = new Commune('r');
+    let ConstructionBuro = new BuroOfConstruction();
+let committee = new CommitteeOfAppropriation();
+let map = new BuroOfCartography();
+let r = map.mapRoom('W41N36');
+let CreepsBuro = new BuroOfProduction(commune);
+let HarvestBuro = new BuroOfHarvest('r');
+let ProgressBuro = new BuroOfProgress('r');
+    commune.init();
+    committee.focusOnCommune('r');
+    CreepsBuro.getProductionFacilities();
+    /*
+    const conSites = Game.rooms['W41N36'].find(FIND_CONSTRUCTION_SITES);
+    for (let site in conSites){
+        conSites[site].remove();
+    }
+    */
   console.log('>>>>>>>>>>>>>> NEW TICK <<<<<<<<<<<<<');
+  r = CreepsBuro.clearDestroyedCreeps();
   if (myCron(100)) {
     ConstructionBuro.placeImportantRoads();
 
   }
-  if (myCron(20)) {
+  if (myCron(10)) {
     committee.transferAssets(Game.spawns.Home.room.find(FIND_SOURCES));
     committee.transferAssets(Game.spawns.Home.room.find(FIND_STRUCTURES, {
       filter: (s) => {
         return s.structureType === STRUCTURE_CONTROLLER
+        ||s.structureType === STRUCTURE_TOWER
+        ||s.structureType === STRUCTURE_EXTENSION
+        ||s.structureType === STRUCTURE_STORAGE
       }
     }));
-    committee.transferAssets(Game.creeps);
     committee.transferAssets(Game.spawns);
     committee.transferAssets(Game.rooms);
   }
-  if (myCron(5)) {
-
+  if (myCron(1)) {
+    committee.transferAssets(Game.creeps);
     //console.log('Removing Creeps:', JSON.stringify(r));
     HarvestBuro.AssignePermaHarvest();
     ConstructionBuro.AssignJobs();
     ProgressBuro.AssignJobs();
   }
-  r = CreepsBuro.clearDestroyedCreeps();
+  
   if (HarvestBuro.needMoreCreeps || ConstructionBuro.needMoreCreeps || ProgressBuro.needMoreCreeps)
     CreepsBuro.produceCreep();
 
